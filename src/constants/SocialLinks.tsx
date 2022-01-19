19a13d2a24377a247ae833ea4b8d2fd2e5cefc47
Kamil Rybiński <kamilryba19@gmail.com>
2022-01-19 18:34:15 +0100

    Update social links

--- src/constants/SocialLinks.tsx
+++ src/constants/SocialLinks.tsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import { FaFacebookSquare, FaLinkedin, FaDribbbleSquare, FaBehanceSquare, FaTwitterSquare } from 'react-icons/fa';
+import { FaLinkedin, FaGithubSquare } from 'react-icons/fa';
 import { SocialLink } from '../types';
 
 const data: SocialLink[] = [
 	{
 		id: 1,
-		icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
-		url: 'https://www.twitter.com',
+		icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+		url: 'https://pl.linkedin.com/in/kamil-rybinski-346b99b1',
 	},
 	{
 		id: 2,
-		icon: <FaLinkedin className="social-icon"></FaLinkedin>,
-		url: 'https://www.twitter.com',
-	},
-	{
-		id: 3,
-		icon: <FaDribbbleSquare className="social-icon"></FaDribbbleSquare>,
-		url: 'https://www.twitter.com',
-	},
-	{
-		id: 4,
-		icon: <FaBehanceSquare className="social-icon"></FaBehanceSquare>,
-		url: 'https://www.twitter.com',
-	},
-	{
-		id: 5,
-		icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
-		url: 'https://www.twitter.com',
+		icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
+		url: 'https://github.com/krybinski',
 	},
 ];
 
