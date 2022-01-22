ba5398bbdcb0e22bbec5ca709fc211c1874db8e7
Kamil Rybiński <kamilryba19@gmail.com>
2022-01-22 17:06:11 +0100

    Small grid improvement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,20 +4,22 @@ import { Link } from 'gatsby';
 const Hero = () => {
 	return (
 		<header className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-			<div className="sm:text-center lg:text-left">
-				<h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-					<span className="block xl:inline">
+			<div className="text-left">
+				<h1 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-5xl lg:text-6xl">
+					<span className="block">
 						Hi{' '}
 						<span role="img" aria-label="twtr">
 							👋
-						</span>
+						</span>{' '}
+						I'm
 					</span>{' '}
-					<span className="block text-indigo-600 xl:inline">I'm Kamil</span>
+					<span className="block text-4xl text-indigo-600 sm:text-6xl md:text-7xl lg:text-8xl">Kamil Rybiński</span>
 				</h1>
-				<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-					Full-stack developer based in Gdansk, Poland. Mainly, I use JavaScript and PHP in my daily work.
+				<p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl md:mt-5 md:text-xl lg:mx-0">
+					I work as a Full Stack Developer. The purpose of creating this blog was to organize the knowledge and improve
+					my English skills.
 				</p>
-				<div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+				<div className="mt-5 sm:mt-8 sm:flex justify-start">
 					<div className="rounded-md shadow">
 						<Link
 							to="/about"
